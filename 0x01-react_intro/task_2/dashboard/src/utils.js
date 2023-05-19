318c2b77a8704de8576f5d88782addbc192357ab
MpiloNM95 <mpilonmafu95@gmail.com>
2023-05-19 14:28:14 +0200

    task 4

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,17 +1,22 @@
 const getFullYear = () => {
 	let current_year = new Date().getFullYear();
 	return current_year;
-}
-
-const getFooterCopy = (isIndex) => {
+  }
+  
+  const getFooterCopy = (isIndex) => {
 	if (isIndex === true) {
-		return "Holberton School";
+	  return "Holberton School";
 	} else {
-		return "Holberton School main dashboard";
+	  return "Holberton School main dashboard";
 	}
-}
-
-module.exports = {
+  }
+  
+  const getLatestNotification = () => {
+	return "<strong>Urgent requirement</strong> - complete by EOD";
+  }
+  
+  module.exports = {
 	getFooterCopy,
 	getFullYear,
+	getLatestNotification
 };
