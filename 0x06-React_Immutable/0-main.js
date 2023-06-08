cb437e40feddfba4f6dfa31416ce566e5fef8154
MpiloNM95 <mpilonmafu95@gmail.com>
2023-06-08 12:55:53 +0200

    task 0

--- 0x06-React_Immutable/0-main.js
+++ 0x06-React_Immutable/0-main.js
@@ -1,12 +1,10 @@
-import getImmutableObject from './0-fromjs';
+import getImmutableObject from './0-fromjs.js';
 
-// Example usage
-const inputObject = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
+const object = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
 };
 
-const immutableMap = getImmutableObject(inputObject);
-console.log(immutableMap);
+console.log(getImmutableObject(object));
