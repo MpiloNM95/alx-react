7ce0bc5fcb209f7bc5c1da03d8c7cfb1609a47ae
MpiloNM95 <mpilonmafu95@gmail.com>
2023-06-14 19:38:23 +0200

    task 1

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,4 +1,4 @@
-import * as notificationsData from "../../../..//notifications.json";
+import * as notificationsData from "../../../../notifications.json";
 import { normalize, schema } from "normalizr";
 
 export function getAllNotificationsByUser(userId) {
