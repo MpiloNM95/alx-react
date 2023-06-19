2d06cdcb8e763abc85074f261ee464deab18279f
MpiloNM95 <mpilonmafu95@gmail.com>
2023-06-19 19:43:44 +0200

    task 4

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -1,4 +1,4 @@
-import * as notificationsData from "../../../../notifications.json";
+import * as notificationsData from "../../notifications.json";
 import { normalize, schema } from "normalizr";
 
 const user = new schema.Entity("users");
@@ -34,3 +34,11 @@ export function getAllNotificationsByUser(userId) {
 
   return notificationsByUser;
 }
+
+const notificationsNormalizer = (data) => {
+  const normalizedData = normalize(data, [notification]);
+
+  return normalizedData.entities;
+};
+
+export default notificationsNormalizer;
