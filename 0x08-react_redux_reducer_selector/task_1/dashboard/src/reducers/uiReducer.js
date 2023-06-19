19e4dab9f847e3034c4d397299b3b2462c0c05f5
MpiloNM95 <mpilonmafu95@gmail.com>
2023-06-19 18:17:48 +0200

    task 1

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,13 +1,6 @@
 import react from React;
-import { LOGIN } from "../actions/uiActionTypes";
-import { LOGOUT } from "../actions/uiActionTypes";
-import { DISPLAY_NOTIFICATION_DRAWER } from "../actions/uiActionTypes";
-import { HIDE_NOTIFICATION_DRAWER } from "../actions/uiActionTypes";
-import { LOGIN_SUCCESS } from "../actions/uiActionTypes";
-import { LOGIN_FAILURE } from "../actions/uiActionTypes";
 
-export default function uiReducer () {
-    isNotificationDrawerVisible : false;
-    isUserLoggedIn: false;
-    user: [];
+function uiReducer() {
 }
+
+export default uiReducer;
